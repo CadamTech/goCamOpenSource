3abc6baf288b48e277fac01f8ca23a47ace293a6
Hamza Zbili <hamza.zbili.92@gmail.com>
2024-06-11 15:30:19 +0200

    permission

--- source/frontend/js/app/avsFactoryIframeSdk.js/v1/02-iframeHandler.ts
+++ source/frontend/js/app/avsFactoryIframeSdk.js/v1/02-iframeHandler.ts
@@ -33,7 +33,7 @@ namespace AvsFactoryIframeSdk {
         this.iframeElement.style.position = "fixed";
         this.iframeElement.style.top = "0";
         this.iframeElement.style.left = "0";
-        this.iframeElement.allow = "camera https://gocam.opale.io";
+        this.iframeElement.allow = "camera microphone https://gocam.opale.io";
         document.body.appendChild(this.iframeElement);
       }
 
