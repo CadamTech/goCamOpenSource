cf056e5383e2d9eec71813480a7ea0bcc25b84ae
Hamza Zbili <hamza.zbili.92@gmail.com>
2024-06-11 14:06:16 +0200

    built for prod

--- app/frontend/static/js/app/avsFactoryIframeSdk.js
+++ app/frontend/static/js/app/avsFactoryIframeSdk.js
@@ -129,11 +129,7 @@ var AvsFactoryIframeSdk;
                 this.iframeElement.style.position = "fixed";
                 this.iframeElement.style.top = "0";
                 this.iframeElement.style.left = "0";
-                this.iframeElement.allow = "camera; microphone; https://gocam.opale.io";
-                this.iframeElement.allow =
-                    "camera; microphone; https://authenticator-dev.opale.io/";
-                this.iframeElement.allow =
-                    "camera; microphone; https://authenticator.opale.io/";
+                this.iframeElement.allow = "camera; microphone;";
                 document.body.appendChild(this.iframeElement);
             };
             IframeHandler.prototype.destroy = function () {
